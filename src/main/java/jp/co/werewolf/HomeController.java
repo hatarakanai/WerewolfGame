c0de9cbe6b88ea203a15407867392a7a7a71a030
舟木 周平 <compileman0@gmail.com>
2016-07-16 18:21:09 +0900

    データベースアクセス

--- src/main/java/jp/co/werewolf/HomeController.java
+++ src/main/java/jp/co/werewolf/HomeController.java
@@ -2,10 +2,17 @@ package jp.co.werewolf;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+
+import jp.co.werewolf.dao.UserTableDao;
+import jp.co.werewolf.dao.UserTableDto;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +24,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private UserTableDao dao;
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	/**
@@ -27,6 +40,14 @@ public class HomeController {
 		logger.info("Welcome home! The client locale is {}.", locale);
 
 		model.addAttribute("name", "S.funaki" );
+
+		List<UserTableDto> userList = dao.getAllRecord();
+
+
+		for(UserTableDto rec : userList){
+			logger.info("UserId_:" + rec.getUserId());
+			logger.info("UserName_:" + rec.getUserName());
+		}
 
 		return "makeRoom";
 	}
